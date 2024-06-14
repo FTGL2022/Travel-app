b0bf28cde0e393c5e5eb5d4b744170752414ac94
Francisco <118365136+FTGL2022@users.noreply.github.com>
2024-06-14 14:40:00 +0100

    Add files via upload

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "../../App.css";
+import "./SignUp.css";
+import person from "./SingUpIMG/person.png"
+import email from "./SingUpIMG/email.png"
+import password from "./SingUpIMG/password.png"
+
+function SignUp() {
+  return (
+    <div className="containerSignUp">
+      <div className="headerSignUp">
+        <div className="textSignUp">SingUp</div>
+        <div className="underlineSignUp"></div>
+      </div>
+      <div className="inputsSignUp">
+        <div className="inputSignUp">
+          <img className="imgInputSignUp" src={person} alt="" />
+          <input className="inputTypeSignUp" type="text" placeholder="Name" />
+        </div>
+
+        <div className="inputSignUp">
+          <img className="imgInputSignUp" src={email} alt="" />
+          <input className="inputTypeSignUp" type="email" placeholder="Email" />
+        </div>
+
+        <div className="inputSignUp">
+          <img className="imgInputSignUp" src={password} alt="" />
+          <input
+            className="inputTypeSignUp"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+      <div className="forgot-passwordSignUp">
+        Lost Password? <span>Click Here!</span>{" "}
+      </div>
+      <div className="submit-containerSignUp">
+        <div className="submitSignUp">Sign Up</div>
+        <div className="submitSignUp">Login</div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
